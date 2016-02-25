04afbcac4a31b53765c52c9d363a848e332fc959
Tinter <a-348@hotmail.com>
2016-02-25 16:34:21 +0100

    Added DP-27 magazines

--- wpn_r_f_cnfg/config.cpp
+++ wpn_r_f_cnfg/config.cpp
@@ -26,4 +26,30 @@ class CfgMagazines
 		initSpeed=875;
 		mass=5;
 	};
+	class 47rnd_dpbar: 20Rnd_762x51_Mag
+	{
+		author="Street-you";
+		scope=2;
+		displayName="$STR_dp_47b_name";
+		displaynameshort="";
+		picture="\wpn_r_f_cnfg\ammo\ammon\bar_dp_47b";
+		count=47;
+		mass=32;
+		descriptionShort="$STR_dp_47b_desc";
+		initSpeed=800;
+	};
+	class 47rnd_dpbar_trs: 20Rnd_762x51_Mag
+	{
+		author="Street-you";
+		scope=2;
+		displayName="$STR_dp_47b_trs_name";
+		displaynameshort="$STR_545mag_trs_sname";
+		picture="\wpn_r_f_cnfg\ammo\ammon\bar_dp_47b_trs";
+		count=47;
+		mass=32;
+		tracersEvery=1;
+		lastRoundsTracer=47;
+		descriptionShort="$STR_dp_47b_trs_desc";
+		initSpeed=800;
+	};
 };
