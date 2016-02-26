636e5a258ff1de2487f03bea9c42214021a14efc
Mallow234 <ashilld@sky.com>
2016-02-26 22:46:18 +0000

    Added PPSh + PPS
    Shapagin wills it
    Also added data neccessary for these to function, also updated
    stringtables slightly

---
+++ wpn_r_f4/config.cpp
@@ -0,0 +1,10 @@
+class CfgPatches
+{
+	class R_4
+	{
+		units[]={};
+		weapons[]={};
+		requiredVersion=0.1;
+		requiredAddons[]={};
+	};
+};
--- wpn_r_f_cnfg/config.cpp
+++ wpn_r_f_cnfg/config.cpp
@@ -13,6 +13,7 @@ class CfgPatches
 };
 class CfgMagazines
 {
+    class CA_Magazine;
 	class 20Rnd_762x51_Mag;
 	class 5rnd_762_mos: 20Rnd_762x51_Mag
 	{
@@ -63,5 +64,89 @@ class CfgMagazines
 		descriptionShort="$STR_svt_10b_desc";
 		initSpeed=810;
 		mass=10;
+	};
+	class 71rnd_psh_bar: CA_Magazine
+	{
+		author="Street-you";
+		scope=2;
+		displayName="$STR_ppsh71_name";
+		displaynameshort="";
+		picture="\wpn_r_f_cnfg\ammon\psh_71b";
+		count=71;
+		ammo="B_762x25_pula";
+		initSpeed=480;
+		tracersEvery=0;
+		lastRoundsTracer=0;
+		descriptionShort="$STR_ppsh71_desc";
+		mass=30;
+	};
+	class 35rnd_psh_mag: CA_Magazine
+	{
+		author="Street-you";
+		scope=2;
+		displayName="$STR_ppsh35_name";
+		displaynameshort="";
+		picture="\wpn_r_f_cnfg\ammon\psh_35b";
+		count=35;
+		ammo="B_762x25_pula";
+		initSpeed=480;
+		tracersEvery=0;
+		lastRoundsTracer=0;
+		descriptionShort="$STR_ppsh35_desc";
+		mass=11;
+	};
+	class 35rnd_psh_trs_mag: 35rnd_psh_mag
+	{
+		author="Street-you";
+		scope=2;
+		displayName="$STR_ppsh35_trs_name";
+		displaynameshort="";
+		picture="\wpn_r_f_cnfg\ammon\psh_trs_35b";
+		ammo="B_762x25_trs_pula";
+		tracersEvery=1;
+		lastRoundsTracer=35;
+		descriptionShort="$STR_ppsh35_trs_desc";
+		mass=11;
+	};
+};
+class CfgAmmo
+{
+    class BulletBase;
+	class B_762x25_pula: BulletBase
+	{
+		hit=5.6500001;
+		indirectHit=0;
+		indirectHitRange=0;
+		cartridge="FxCartridge_9mm";
+		cost=100;
+		typicalSpeed=470;
+		airFriction=-0.0016;
+		caliber=1.7;
+		deflecting=45;
+		model="\A3\Weapons_f\Data\bullettracer\tracer_yellow";
+		tracerScale=0.5;
+		tracerStartTime=0.050000001;
+		tracerEndTime=1;
+		nvgOnly=0;
+		visibleFire=2;
+		audibleFire=4;
+		class CamShakeExplode
+		{
+			power="(2.62^0.5)";
+			duration="((round (2.62^0.5))*0.2 max 0.2)";
+			frequency=20;
+			distance="((2.62^0.5)*3)";
+		};
+		class CamShakeHit
+		{
+			power=3;
+			duration="((round (2.62^0.25))*0.2 max 0.2)";
+			frequency=20;
+			distance=1;
+		};
+	};
+	class B_762x25_trs_pula: B_762x25_pula
+	{
+		model="\A3\Weapons_f\Data\bullettracer\tracer_green";
 	};
 };
