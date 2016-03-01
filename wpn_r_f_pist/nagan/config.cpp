c3a32945b1d4bc7be9942b26f16b8042ea833f7f
Tinter <a-348@hotmail.com>
2016-03-01 23:51:46 +0100

    Changed nagant cfgPatches and removed useless inherits.

--- wpn_r_f_pist/nagan/config.cpp
+++ wpn_r_f_pist/nagan/config.cpp
@@ -3,26 +3,17 @@ class CfgPatches
 	class weap_StreetYou_nagan
 	{
 		units[]={};
-		weapons[]={};
+		weapons[]={
+			"str_ngn"
+		};
 		requiredVersion=0.1;
+		requiredAddons[]=
+		{
+			"A3_Weapons_F"
+		};
 	};
 };
 class Mode_SemiAuto;
-class Mode_Burst;
-class Mode_FullAuto;
-class single_close_optics1;
-class single_medium_optics1;
-class single_far_optics1;
-class single_far_optics2;
-class fullauto_medium;
-class SlotInfo;
-class ItemCore;
-class InventoryItem_Base_F;
-class InventoryMuzzleItem_Base_F;
-class InventoryOpticsItem_Base_F;
-class CowsSlot;
-class PointerSlot;
-class MuzzleSlot;
 class CfgWeapons
 {
 	class Pistol;
